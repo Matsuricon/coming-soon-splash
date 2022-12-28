d4b172f81dd5ebdbe489f28cd9fdac25b480475d
Callie Borgerding <108496882+calpicomai@users.noreply.github.com>
2022-12-28 00:07:47 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="under_construction.png" className="App-logo" alt="logo" />
+        <p>
+          Matsuricon <span className="heart">♥️</span> React
+        </p>
+        <p className="small">
+          We are currently building something amazing and it will be released soon! Stay tuned...
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
